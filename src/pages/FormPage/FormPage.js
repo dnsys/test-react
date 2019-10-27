bb275a6c63cb462d6b237adec2c24f407ba0b262
dnnsys <shout2794@gmail.com>
2019-10-27 14:49:51 +0200

    feat: base grid of form page. removed extra files css

--- src/pages/FormPage/FormPage.js
+++ src/pages/FormPage/FormPage.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap';
+import './style.scss';
 
 class FormPage extends Component{
     render() {
         return (
-            <div>
-                Hello! I am form page
+            <div className="form-page">
+                <Container>
+                    <Row>
+                        <Col md={4}>
+                            Col md 4
+                        </Col>
+                        <Col md={8}>
+                            Col md 8
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
