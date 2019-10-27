cf0b24f7ca6d54edc1aa9fe7729b4408d7fabcf9
dnnsys <shout2794@gmail.com>
2019-10-27 21:17:10 +0200

    feat: removed plugin datepicker

--- src/components/FormPanel/FormPanel.js
+++ src/components/FormPanel/FormPanel.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
-import DayPicker from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
 
 class FormPanel extends Component{
-    constructor(props) {
-        super(props);
-        this.handleDayClick = this.handleDayClick.bind(this);
-        this.state = {
-            selectedDay: null,
-        };
-    }
-
-    handleDayClick(day, { selected }) {
-        this.setState({
-            selectedDay: selected ? undefined : day,
-        });
-    }
-
     render() {
         return (
             <div className="form-block">
@@ -25,20 +9,7 @@ class FormPanel extends Component{
                 </div>
                 <div className="form-block__container">
                     <div className="form-block__datepicker">
-                        <DayPicker
-                            initialMonth={new Date(2019, 9)}
-                            disabledDays={[
-                                new Date(2019, 9, 12),
-                                new Date(2019, 9, 2),
-                                {
-                                    after: new Date(2019, 9, 20),
-                                    before: new Date(2019, 9, 25),
-                                },
-                                { daysOfWeek: [0, 6] }
-                            ]}
-                            selectedDays={this.state.selectedDay}
-                            onDayClick={this.handleDayClick}
-                        />
+
                     </div>
                 </div>
             </div>
