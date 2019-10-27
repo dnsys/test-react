c20a391e49ce64835d73a5fea320b7a3e9979fac
dnnsys <shout2794@gmail.com>
2019-10-27 14:19:17 +0200

    chore: got set up router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-//import components
-import {Button} from './components/Button/index';
-import {Calendar} from './components/Calendar/index';
-import {Form} from './components/Form/index';
-import {Sidebar} from './components/Sidebar/index';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button></Button>
-        <Calendar></Calendar>
-        <Form></Form>
-        <Sidebar></Sidebar>
-    </div>
-  );
-}
+import logo from './logo.svg';
+
+//import component pages
+import { Home, FormPage } from './pages/index';
+
+const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Route exact path="/" component={Home} />
+                <Route exact path="/form" component={FormPage} />
+            </div>
+        </Router>
+    );
+};
 
 export default App;
