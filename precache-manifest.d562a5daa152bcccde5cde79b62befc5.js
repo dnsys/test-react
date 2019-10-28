7412b6d7258a8a7993d5b7a79b4dfb65f4e7cb65
dnnsys <shout2794@gmail.com>
2019-10-28 20:59:42 +0200

    Updates

---
+++ precache-manifest.d562a5daa152bcccde5cde79b62befc5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bbabffb9f3691a0dc6ad8f2bbe854f",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "0ff373f418ceaa57dfd8",
+    "url": "/test-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3a02666254dab9062797",
+    "url": "/test-react/static/css/main.284d1a58.chunk.css"
+  },
+  {
+    "revision": "0ff373f418ceaa57dfd8",
+    "url": "/test-react/static/js/2.9412b403.chunk.js"
+  },
+  {
+    "revision": "3a02666254dab9062797",
+    "url": "/test-react/static/js/main.7704e7ff.chunk.js"
+  },
+  {
+    "revision": "271b2ec17f58114b67be",
+    "url": "/test-react/static/js/runtime-main.2d50cf39.js"
+  },
+  {
+    "revision": "0188899cd3ec92cdcaa4c97ca0c75247",
+    "url": "/test-react/static/media/ProximaNova-Light.0188899c.ttf"
+  },
+  {
+    "revision": "d778218590f14376c366c515d384808e",
+    "url": "/test-react/static/media/ProximaNova-Light.d7782185.eot"
+  },
+  {
+    "revision": "eb74a591665045d87eabfb2dc759be0a",
+    "url": "/test-react/static/media/ProximaNova-Light.eb74a591.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-react/static/media/logo.25bf045c.svg"
+  }
+]);
